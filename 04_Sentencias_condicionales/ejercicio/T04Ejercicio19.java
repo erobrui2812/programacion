24ceaeff01f580a09e13c608011c79df3cc6d632
erobrui2812 <erobrui2812@g.educaand.es>
2023-11-09 18:22:06 +0100

    arreglo

--- 04_Sentencias_condicionales/ejercicio/T04Ejercicio19.java
+++ 04_Sentencias_condicionales/ejercicio/T04Ejercicio19.java
@@ -11,6 +11,14 @@ public class T04Ejercicio19 {
     System.out.print("Ingresa un número entero (positivo o negativo de hasta 5 dígitos): ");
     int numeroUsuario = sc.nextInt();
     
+    int largo = String.valueOf(Math.abs(numeroUsuario)).length();
+
+    if (largo >= 6) {
+      System.out.println("No es válido");
+    } else {
+      System.out.println("El largo del digito introducido es de "+largo+" digitos.");
+
+    }
 
     sc.close();
   }
