fa8451208a2e49499e44febeb648fc8a5c5a5ead
erobrui2812 <erobrui2812@g.educaand.es>
2023-11-12 17:01:43 +0100

    Subida de ejercicios 23 y 24

--- 04_Sentencias_condicionales/ejercicio/T04Ejercicio23.java
+++ 04_Sentencias_condicionales/ejercicio/T04Ejercicio23.java
@@ -1,3 +1,74 @@
+/**
+*
+* @author Elías Robles
+*/
+import java.util.Scanner;
 public class T04Ejercicio23 {
-  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    System.out.print("Introduzca la base imponible: ");
+    double baseImponible = sc.nextDouble();
+    sc.nextLine();
+    System.out.print("Introduzca el tipo de IVA (general, reducido o superreducido): ");
+    String tipoIVA = sc.nextLine().toLowerCase();
+
+    System.out.print("Introduce el código promocional (nopro, mitad, meno5 o 5porc): ");
+    String codigoPromocional = sc.nextLine().toLowerCase();
+
+    int porcentajeIVA = 0;
+
+    switch (tipoIVA) {
+      case "general":
+        porcentajeIVA = 21;
+        break;
+      
+      case "reducido":
+        porcentajeIVA = 10;
+        break;
+      
+      case "superreducido":
+        porcentajeIVA = 4;
+        break;
+      
+      default:
+        System.out.println("No has introducido un valor correcto de IVA");
+        break;
+    }
+
+    double iva = baseImponible * porcentajeIVA / 100;
+    double precioIva = baseImponible + iva;
+
+    double descuento = 0;
+    switch (codigoPromocional) {
+      case "nopro":
+        descuento = 0;
+        break;
+      
+      case "mitad":
+        descuento = baseImponible / 2;
+        break;
+      
+      case "meno5":
+        descuento = 5;
+        break;
+
+      case "5porc":
+        descuento = baseImponible * (5 / 100);
+        break;
+    
+      default:
+        System.out.println("No has introducido un valor correcto de código promocional");
+        break;
+    }
+
+    double total = precioIva - descuento;
+    
+    System.out.printf("%-20s %6.2f\n","Base imponible", baseImponible);
+    System.out.printf("IVA (%2d%%)            %6.2f\n", porcentajeIVA, iva);
+    System.out.printf("%-20s %6.2f\n","Precio con IVA", precioIva);
+    System.out.printf("Cód. promo. (%5s) -%6.2f\n", codigoPromocional, descuento);
+    System.out.printf("%-20s %6.2f\n","TOTAL", total);
+
+    sc.close();
+  }
 }
--- 04_Sentencias_condicionales/ejercicio/T04Ejercicio24.java
+++ 04_Sentencias_condicionales/ejercicio/T04Ejercicio24.java
@@ -1,3 +1,69 @@
+import java.util.Scanner;
+
+/**
+ * Description: Program to calculate the salary of an employee based on their position, travel days, and marital status.
+ * Author: Elías Robles
+ */
 public class T04Ejercicio24 {
-  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("1 - Programador junior");
+    System.out.println("2 - Prog. senior");
+    System.out.println("3 - Jefe de proyecto");
+    System.out.print("Introduzca el cargo del empleado (1 - 3): ");
+    int cargo = sc.nextInt();
+
+    System.out.print("¿Cuántos días ha estado de viaje visitando clientes? ");
+    int diasViaje = sc.nextInt();
+
+    System.out.print("Introduzca su estado civil (1 - Soltero, 2 - Casado): ");
+    int estadoCivil = sc.nextInt();
+
+    sc.close();
+
+    double sueldoBase = 0.0;
+    int sueldoDietas = 30;
+    double dietas = diasViaje * sueldoDietas;
+
+    switch (cargo) {
+      case 1:
+        sueldoBase = 950.0;
+        break;
+
+      case 2:
+        sueldoBase = 1200.0;
+        break;
+
+      case 3:
+        sueldoBase = 1600.0;
+        break;
+
+      default:
+        System.out.println("Cargo inválido");
+    }
+
+    int IRPF = 0;
+    switch (estadoCivil) {
+      case 1:
+        IRPF = 25;
+        break;
+
+      case 2:
+        IRPF = 20;
+        break;
+
+      default:
+        System.out.println("Estado civil inválido");
+    }
+
+    double sueldoBruto = sueldoBase + dietas;
+    double retencion = (sueldoBruto * IRPF) / 100;
+    double sueldoNeto = sueldoBruto - retencion;
+    System.out.printf("\n%-20s %.2f", "Sueldo base", sueldoBase);
+    System.out.printf("\n%-6s (%-1d viajes)      %.2f", "Dietas", diasViaje, dietas);
+    System.out.printf("\n\n%-20s %.2f", "Sueldo bruto", sueldoBruto);
+    System.out.printf("\n%-20s %.2f", "Retención IRPF", retencion);
+    System.out.printf("\n\n%-20s %.2f", "Sueldo neto", sueldoNeto);
+  }
 }
