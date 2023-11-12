c8e2629eefdfcd0f6d3d1ff92924fb9b87c3fd72
erobrui2812 <erobrui2812@g.educaand.es>
2023-11-12 15:53:55 +0100

    Modificacion del T04Ejercicio22

--- 04_Sentencias_condicionales/ejercicio/T04Ejercicio22.java
+++ 04_Sentencias_condicionales/ejercicio/T04Ejercicio22.java
@@ -8,22 +8,41 @@ public class T04Ejercicio22 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("Dime el dia de la semana en número (Lunes a Viernes): ");
-    int diaSemana = sc.nextInt();
+    System.out.print("Dime el dia de la semana en número (Lunes a Viernes): €");
+    String diaSemana = sc.nextLine();
 
-    if ((diaSemana >=1)&&(diaSemana <=7)) {
-      System.out.print("Dime la hora y los minutos separados por espacio: ");
-      int horas = sc.nextInt();
-      int minutos = sc.nextInt();
-      int minutosEnUnDia = 24 * 60;
-      int minutosHoy = (horas * 60) + minutos;
-      int minutosRestantesHoy = minutosEnUnDia - minutosHoy;
-      int minutosHastaFinDeSemana += ((5 - diaSemana) % 5) * minutosEnUnDia;
+    final int MINUTOSTOTALES = (4*24*60) + (15*60);
+    int diasTranscurridos = 0;
+    switch (diaSemana) {
+      case "Lunes":
+        diasTranscurridos=0;
+        break;
       
-    } else {
-      System.out.println("No has introducido un dia válido");
-    }
-    
-    sc.close();
-  }  
-}
+      case "Martes":
+        diasTranscurridos=1;
+        break;
+
+      case "Miercoles":
+        diasTranscurridos=2;
+        break;  
+
+      case "Jueves":
+        diasTranscurridos=3;
+        break;
+
+      case "Viernes":
+        diasTranscurridos=4;
+        break;
+
+      default:
+        System.out.println("No has introducido un dia válido");
+        break;
+      }
+      System.out.println("Introduce una hora y minutos separados por espacios:");
+      int hora = sc.nextInt();
+      int minutos = sc.nextInt();
+      int minutosTranscurridos = (diasTranscurridos * 24 * 60) + (hora *60) + minutos;
+      System.out.println("Faltan " + (MINUTOSTOTALES-minutosTranscurridos) + " minutos para llegar a fin de semana.");
+      sc.close();
+  }
+}  
