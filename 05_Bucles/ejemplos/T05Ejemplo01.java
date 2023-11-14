21f822dd566f591ccd0a464f13cc22bdaff46316
erobrui2812 <erobrui2812@g.educaand.es>
2023-11-14 15:51:34 +0100

    Tema 5 Bucles

---
+++ 05_Bucles/ejemplos/T05Ejemplo01.java
@@ -0,0 +1,13 @@
+/**
+* Ejemplo bucle for
+* @author Elías Robles
+*/
+public class T05Ejemplo01 {
+  public static void main(String[] args) {
+    int max = 10;
+
+    for (int i = 1; i < max+1; i++) {
+      System.out.println("El numero es "+i);
+    }
+  }
+}
