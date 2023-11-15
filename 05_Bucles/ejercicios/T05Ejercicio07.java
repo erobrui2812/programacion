81bcf1ae26019b211c2a08de404781ebd5fce6e9
erobrui2812 <erobrui2812@g.educaand.es>
2023-11-15 21:12:29 +0100

    Ejercicios T05

--- 05_Bucles/ejercicios/T05Ejercicio07.java
+++ 05_Bucles/ejercicios/T05Ejercicio07.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class T05Ejercicio07 {
     public static void main(String args[]) {
-        int password = 1234;
+        final int password = 1234;
         int passwordIntroducida;
         Scanner sc = new Scanner(System.in);
 
         int contador = 1;
-        int maxIntentos = 4;
-
-        while (contador <= maxIntentos){
+        final int maxIntentos = 4;
+        boolean fin = false;
+        
+        while ((contador <= maxIntentos) && (!fin)){
             System.out.println("Dime la contraseña");
             passwordIntroducida = sc.nextInt();
 
             if(passwordIntroducida == password){
                 System.out.println("La caja fuerte se ha abierto satisfactoriamente");
-                contador=maxIntentos;
+                fin=true;
             }
             else{
                 System.out.println("Lo siento, esa no es la combinación");
--- 05_Bucles/ejercicios/T05Ejercicio08.java
+++ 05_Bucles/ejercicios/T05Ejercicio08.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class T05Ejercicio08 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dime un número y te mostraré su tabla de multiplicar:");
+        System.out.print("Dime un número y te mostraré su tabla de multiplicar: ");
         int numero = sc.nextInt();
         sc.nextLine();
-
+        System.out.println("");
         int valorActual;
         int contador = 0;
         int maxMultiplicacion = 10;
@@ -14,7 +14,7 @@ public class T05Ejercicio08 {
         while (contador < maxMultiplicacion) {
             contador++;
             valorActual = numero * contador;
-            System.out.println(valorActual);
+            System.out.printf("%3d x %3d = %-8d\n",numero,contador,valorActual);
         }
 
         sc.close();
